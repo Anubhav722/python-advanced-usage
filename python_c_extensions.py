62bdc53d7f84e6abca328171c95194d244f8a99b
Anubhav722 <anubhav@launchyard.com>
2017-12-08 09:56:26 +0000

    add file for cgi, crypto, c_extensions for python

---
+++ python_c_extensions.py
@@ -0,0 +1,6 @@
+# About extending python with C
+
+# WHAT ARE C EXTENSIONS
+# Python talking with the C Programming language.
+# Creation of wrappers which bind python objects to C Functions.
+# There is many sub-topics of C extensions.
---
+++ python_cgi.py
@@ -0,0 +1,7 @@
+# CGI stands for Common Gateway Interface
+# CGI is the standard for programs to interface with HTTP Servers.
+# This means we can take information from things like forms.
+
+# Let's setup a simple default web server on Ubuntu Apache 2
+
+import socket
